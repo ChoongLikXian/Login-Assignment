ed4f95536e3a02924a3fcbdd259d4909dfeb38f5
Lik Xian <xian_1998@hotmail.com>
2020-09-16 21:14:24 +0800

    By : Choong Lik Xian

---
+++ app/src/main/java/com/example/login2/MainpageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.login2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainpageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mainpage)
+    }
+}
